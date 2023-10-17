d5a175e3b81175c6d4162141fd3fb356f453926c
tzs919 <tzs919@163.com>
2023-10-17 15:57:43 +0800

    t

--- src/main/java/app/MyXmlApp.java
+++ src/main/java/app/MyXmlApp.java
@@ -1,21 +1,17 @@
 package app;
 
-import concert.Encoreable;
 import concert.Performance;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyXmlApp {
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:META-INF/spring/concert5.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:META-INF/spring/concert2.xml");
 
-//        Performance concert = ctx.getBean("concert", Performance.class);
-//        concert.perform();
-
-        Encoreable concert = ctx.getBean("concert", Encoreable.class);
-        concert.performEncore();
-//
-//        Encoreable concert2 = ctx.getBean("concert2", Encoreable.class);
-//        concert2.performEncore();
+        Performance concert = ctx.getBean("concert", Performance.class);
+        concert.perform();
     }
 }
+
+//        Encoreable concert = ctx.getBean("concert", Encoreable.class);
+//        concert.performEncore();
--- src/main/java/concert/Concert.java
+++ src/main/java/concert/Concert.java
@@ -1,23 +1,8 @@
 package concert;
 
 public class Concert implements Performance {
-    private String myname = "taozhasoheng";
-
-    public String getMyname() {
-        return myname;
-    }
-
-    public void setMyname(String myname) {
-        this.myname = myname;
-    }
-
     @Override
     public void perform() {
         System.out.println("perform...");
-    }
-
-    @Override
-    public String toString() {
-        return "taozs";
     }
 }
